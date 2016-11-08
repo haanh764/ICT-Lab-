ecd870a3c150428488ad2c781ffd8f4b08b40888
haanh764 <ngohaanh764@gmail.com>
2016-11-09 04:42:11 +0700

    Done dashboard

---
+++ application/views/backend/admin/dashboard.php
@@ -0,0 +1,165 @@
+<div class="row">
+	<div class="col-md-8">
+    	<div class="row">
+            <!-- CALENDAR-->
+            <div class="col-md-12 col-xs-12">    
+                <div class="panel panel-primary " data-collapsed="0">
+                    <div class="panel-heading">
+                        <div class="panel-title">
+                            <i class="fa fa-calendar"></i>
+                            <?php echo get_phrase('event_schedule');?>
+                        </div>
+                    </div>
+                    <div class="panel-body" style="padding:0px;">
+                        <div id="calendar"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+	<div class="col-md-4">
+		<div class="row">
+            <div class="col-md-12">
+            
+                <div class="tile-stats tile-red">
+                    <div class="icon"><i class="fa fa-group"></i></div>
+                    <div class="num" data-start="0" data-end="<?php echo $this->db->count_all('student');?>" 
+                    		data-postfix="" data-duration="1500" data-delay="0">0</div>
+                    
+                    <h3><?php echo get_phrase('student');?></h3>
+                   <p>Total students</p>
+                </div>
+                
+            </div>
+            <div class="col-md-12">
+            
+                <div class="tile-stats tile-green">
+                    <div class="icon"><i class="entypo-users"></i></div>
+                    <div class="num" data-start="0" data-end="<?php echo $this->db->count_all('teacher');?>" 
+                    		data-postfix="" data-duration="800" data-delay="0">0</div>
+                    
+                    <h3><?php echo get_phrase('teacher');?></h3>
+                   <p>Total teachers</p>
+                </div>
+                
+            </div>
+            <div class="col-md-12">
+            
+                <div class="tile-stats tile-aqua">
+                    <div class="icon"><i class="entypo-user"></i></div>
+                    <div class="num" data-start="0" data-end="<?php echo $this->db->count_all('parent');?>" 
+                    		data-postfix="" data-duration="500" data-delay="0">0</div>
+                    
+                    <h3><?php echo get_phrase('parent');?></h3>
+                   <p>Total parents</p>
+                </div>
+                
+            </div>
+    	</div>
+    </div>
+	
+</div>
+
+
+
+    <script>
+  $(document).ready(function() {
+	  
+	  var calendar = $('#notice_calendar');
+				
+				$('#notice_calendar').fullCalendar({
+					header: {
+						left: 'title',
+						right: 'today prev,next'
+					},
+					
+					//defaultView: 'basicWeek',
+					
+					editable: false,
+					firstDay: 1,
+					height: 530,
+					droppable: false,
+					
+					events: [
+						<?php 
+						$notices	=	$this->db->get('noticeboard')->result_array();
+						foreach($notices as $row):
+						?>
+						{
+							title: "<?php echo $row['notice_title'];?>",
+							start: new Date(<?php echo date('Y',$row['create_timestamp']);?>, <?php echo date('m',$row['create_timestamp'])-1;?>, <?php echo date('d',$row['create_timestamp']);?>),
+							end:	new Date(<?php echo date('Y',$row['create_timestamp']);?>, <?php echo date('m',$row['create_timestamp'])-1;?>, <?php echo date('d',$row['create_timestamp']);?>) 
+						},
+						<?php 
+						endforeach
+						?>
+						
+					]
+				});
+	});
+  </script>
+  <script type="text/javascript">
+    $.getScript('http://arshaw.com/js/fullcalendar-1.6.4/fullcalendar/fullcalendar.min.js',function(){
+  
+  var date = new Date();
+  var d = date.getDate();
+  var m = date.getMonth();
+  var y = date.getFullYear();
+  
+  $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    editable: true,
+    events: [
+      {
+        title: 'Meeting SWARMS Project',
+        start: new Date(y, m, 1)
+      },
+      {
+        title: 'Seminar 1',
+        start: new Date(y, m, d-5),
+        end: new Date(y, m, d-2)
+      },
+      {
+        id: 999,
+        title: 'Seminar 2',
+        start: new Date(y, m, d-3, 16, 0),
+        allDay: false
+      },
+      {
+        id: 999,
+        title: 'Research Discussion',
+        start: new Date(y, m, d+4, 16, 0),
+        allDay: false
+      },
+      {
+        title: 'Meeting',
+        start: new Date(y, m, d, 10, 30),
+        allDay: false
+      },
+      {
+        title: 'Seminar 3',
+        start: new Date(y, m, d, 12, 0),
+        end: new Date(y, m, d, 14, 0),
+        allDay: false
+      },
+      {
+        title: 'Seminar 4',
+        start: new Date(y, m, d+1, 19, 0),
+        end: new Date(y, m, d+1, 22, 30),
+        allDay: false
+      },
+      {
+        title: 'Research Discussion',
+        start: new Date(y, m, 28),
+        end: new Date(y, m, 29),
+        allDay: false
+      }
+    ]
+  });
+})
+</script>  
